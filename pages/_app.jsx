584f0f5688393b5ea95512254455b77b63e0cf89
Abdulrahim2567 <kibuh73785569@gmail.com>
2023-03-30 13:58:44 +0100

    added some theming modules

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,22 +1,31 @@
 import Head from 'next/head'
 import NextNProgress from 'nextjs-progressbar'
+import { ThemeProvider, createTheme } from "@mui/material/styles"
 
-
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#2a9461"
+		}
+	}
+})
 
 const App = ({ Component, pageProps }) => {
 	return (
 		<Fragment>
-			<Head>
-				<meta name='viewport' content='width=device-width, initial-scale=1' />
-			</Head>
-			<NextNProgress
-				color="#0072F5"
-				startPosition={0.3}
-				stopDelayMs={200}
-				showOnSShallow ={true}
-				height={3}
-			/>
-			<Component {...pageProps} />
+			<ThemeProvider theme={theme}>
+				<Head>
+					<meta name='viewport' content='width=device-width, initial-scale=1' />
+				</Head>
+				<NextNProgress
+					color="#0072F5"
+					startPosition={0.3}
+					stopDelayMs={200}
+					showOnSShallow ={true}
+					height={3}
+					/>
+				<Component {...pageProps} />
+			</ThemeProvider>
 		</Fragment>
 	)
 }
