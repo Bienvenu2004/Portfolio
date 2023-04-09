e8c4465dd307cddc17f2d3e7c5033f2330bc5366
abdulrahim2567 <kibuh73785569@gmail.com>
2023-04-09 18:28:46 +0100

    moved to a src directory

--- src/pages/github/index.jsx
+++ src/pages/github/index.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { useTheme } from "@mui/material"
 
 const Index = () => {
-	
+
 	const theme = useTheme()
 
 	return (
 		<div className='app' style={{
-			padding: "1.2rem 1.2rem 1.2rem 1.5rem",
+			padding: "1.2rem 1.2rem 1.5rem",
 			color: theme.palette.secondary.text
 		}}>
 			GitHub Page
