6521ff4fdbce9973b4fd433ae58aa45f0b7edbb6
Abdulrarhim2567 <kibuh73785569@gmail.com>
2023-04-19 08:57:16 +0100

    added cors to api/portfolio

--- src/pages/api/portfolioapi.js
+++ src/pages/api/portfolioapi.js
@@ -1,10 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import Cors from "cors"
 import { connectDatabase ,insertDocument, getAllDocuments } from "@/lib/mongodb"
+import initMiddleware from "@/lib/initMiddleware"
+
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+    // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+    Cors({
+        // Only allow requests with GET, POST and OPTIONS
+        origin: '*', // allow to server to accept request from different origin
+        methods: ['POST', 'GET'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true, // allow session cookie from browser to pass through
+    })
+);
 
 
 export default async function handler(req, res) {
 
+    await cors(req, res)
+    
     const client = await connectDatabase()
     let documents = []
     const {collection, document, sort} = req.body
