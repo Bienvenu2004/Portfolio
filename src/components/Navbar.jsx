f42a224974588a3db950c343425c674baa0d2e29
Abdulrahim2567 <kibuh73785569@gmail.com>
2023-04-23 16:55:24 +0100

    added profile image in sidebarr

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -93,6 +93,31 @@ const Navbar = ({
                     {!isSidebarOpen && (
                         <IconButton
                             onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+                            sx={{
+                                display: "flex",
+                                transition: "all 0.5s ease-in-out",
+                                color: theme.palette.secondary.main,
+                                backgroundColor:
+                                    theme.palette.mode === "dark" &&
+                                    "rgba(69, 90, 100, 0.1)",
+                                borderRadius: "0.8rem",
+                                border:
+                                    theme.palette.mode === "dark"
+                                        ? "1px solid rgba(1, 87, 155, 0.8)"
+                                        : "1px solid rgba(1, 87, 155, 0.2)",
+                                "&:hover": {
+                                    backgroundColor:
+                                        theme.palette.mode === "dark"
+                                            ? "rgba(1, 87, 155, 0.4)"
+                                            : theme.palette.secondary.main,
+                                    transition: "all 0.5s ease-in-out",
+                                    "& svg": {
+                                        color:
+                                            theme.palette.mode === "light" &&
+                                            "#FFF",
+                                    },
+                                },
+                            }}
                         >
                             <MenuIcon />
                         </IconButton>
