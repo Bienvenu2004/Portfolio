b10352825e5dccb6434aeaed05e65afa5b8005a9
Abdulrahim2567 <kibuh73785569@gmail.com>
2023-04-28 09:54:14 +0100

    skills display box on the skill page

--- src/pages/skills/index.jsx
+++ src/pages/skills/index.jsx
@@ -1,15 +1,26 @@
 import axios from "axios";
-import { useTheme, Box, useMediaQuery } from "@mui/material";
+import {
+    useTheme,
+    Box,
+    useMediaQuery,
+    CircularProgress,
+    Card,
+    CardContent,
+    Typography,
+    IconButton,
+} from "@mui/material";
 import { useEffect } from "react";
 import { getAllDocuments } from "@/lib/mongodbHelper";
 import { Skeleton } from "@mui/material";
+import { Statistic } from "antd";
+import { ArrowUpwardOutlined } from "@mui/icons-material";
 
-const Index = ({ javascript, css, database }) => {
+const Index = ({ javascript = "1", css = 1, database = 1 }) => {
     const matches = useMediaQuery("(min-width:1366px)", { noSsr: true });
     const theme = useTheme();
-    console.log("JS", javascript);
-    console.log("CSS", css);
-    console.log("DATABASE", database);
+    // console.log("JS", javascript);
+    // console.log("CSS", css);
+    // console.log("DATABASE", database);
 
     return (
         <div
@@ -24,7 +35,7 @@ const Index = ({ javascript, css, database }) => {
                 mx={0.75}
                 height="100%"
                 borderRadius={3}
-                backgroundColor={theme.palette.background.alt}
+                // backgroundColor={theme.palette.background.alt}
                 boxShadow={
                     theme.palette.mode === "light" &&
                     "0px 0px 2px 0px rgba(0,0,0,0.2)"
@@ -43,34 +54,219 @@ const Index = ({ javascript, css, database }) => {
                         }}
                     />
                 ) : (
-                    <div>content</div>
+                    <Box
+                        width="100%"
+                        height="120px"
+                        borderRadius={3}
+                        boxShadow={
+                            theme.palette.mode === "light" &&
+                            "0px 0px 2px 0px rgba(0,0,0,0.2)"
+                        }
+                        display="flex"
+                        justifyContent="space-evenly"
+                        padding="10px"
+                    >
+                        <Box
+                            width="24%"
+                            height="inherit"
+                            borderRadius={3}
+                            backgroundColor={theme.palette.background.alt}
+                            display="flex"
+                        >
+                            <Box height="100%" flexGrow={1}>
+                                <Card
+                                    style={{ height: "100%" }}
+                                    sx={{
+                                        backgroundColor: "transparent",
+                                        borderRadius: 3,
+                                        p: 1,
+                                    }}
+                                >
+                                    <Box
+                                        sx={{
+                                            p: 0,
+                                            display: "flex",
+                                            flexDirection: "column",
+                                            height: "100%",
+                                        }}
+                                    >
+                                        <Box
+                                            height="fit-content"
+                                            display="flex"
+                                        >
+                                            <Typography
+                                                variant="h6"
+                                                component="div"
+                                                sx={{
+                                                    pl: 1,
+                                                }}
+                                            >
+                                                JavaScript
+                                            </Typography>
+                                        </Box>
+                                        <Box
+                                            flexGrow={1}
+                                            display="flex"
+                                            alignItems="flex-end"
+                                        >
+                                            <Typography
+                                                variant="h6"
+                                                component="div"
+                                                display="inline-block"
+                                            >
+                                                <Box display="inline-block">
+                                                    <IconButton
+                                                        sx={{
+                                                            mt: 4,
+                                                        }}
+                                                    >
+                                                        <ArrowUpwardOutlined
+                                                            sx={{
+                                                                color: theme
+                                                                    .palette
+                                                                    .secondary
+                                                                    .main,
+                                                                fontSize:
+                                                                    "1.8rem",
+                                                            }}
+                                                        />
+                                                        <Typography
+                                                            display="inline-block"
+                                                            variant="h2"
+                                                            sx={{
+                                                                color: theme
+                                                                    .palette
+                                                                    .secondary
+                                                                    .main,
+                                                                mt: "6px",
+                                                                px: 1,
+                                                            }}
+                                                        >
+                                                            75
+                                                        </Typography>
+                                                    </IconButton>
+                                                </Box>
+                                            </Typography>
+                                        </Box>
+                                    </Box>
+                                </Card>
+                            </Box>
+                        </Box>
+                        <Box
+                            width="24%"
+                            height="inherit"
+                            borderRadius={3}
+                            backgroundColor={theme.palette.background.alt}
+                            display="flex"
+                        >
+                            <Box height="100%" flexGrow={1}>
+                                <Card
+                                    style={{ height: "100%" }}
+                                    sx={{
+                                        backgroundColor: "transparent",
+                                        borderRadius: 3,
+                                        p: 1,
+                                    }}
+                                >
+                                    <Statistic
+                                        title="JavaScript"
+                                        value={78}
+                                        precision={2}
+                                        valueStyle={{
+                                            color: theme.palette.secondary.main,
+                                        }}
+                                        prefix={<ArrowUpwardOutlined />}
+                                        suffix="%"
+                                    />
+                                </Card>
+                            </Box>
+                        </Box>
+                        <Box
+                            width="24%"
+                            height="inherit"
+                            borderRadius={3}
+                            backgroundColor={theme.palette.background.alt}
+                            display="flex"
+                        >
+                            <Box height="100%" flexGrow={1}>
+                                <Card
+                                    style={{ height: "100%" }}
+                                    sx={{
+                                        backgroundColor: "transparent",
+                                        borderRadius: 3,
+                                        p: 1,
+                                    }}
+                                >
+                                    <Statistic
+                                        title="JavaScript"
+                                        value={78}
+                                        precision={2}
+                                        valueStyle={{
+                                            color: theme.palette.secondary.main,
+                                        }}
+                                        prefix={<ArrowUpwardOutlined />}
+                                        suffix="%"
+                                    />
+                                </Card>
+                            </Box>
+                        </Box>
+                        <Box
+                            width="24%"
+                            height="inherit"
+                            borderRadius={3}
+                            backgroundColor={theme.palette.background.alt}
+                            display="flex"
+                        >
+                            <Box height="100%" flexGrow={1}>
+                                <Card
+                                    style={{ height: "100%" }}
+                                    sx={{
+                                        backgroundColor: "transparent",
+                                        borderRadius: 3,
+                                        p: 1,
+                                    }}
+                                >
+                                    <Statistic
+                                        title="JavaScript"
+                                        value={78}
+                                        precision={2}
+                                        valueStyle={{
+                                            color: theme.palette.secondary.main,
+                                        }}
+                                        prefix={<ArrowUpwardOutlined />}
+                                        suffix="%"
+                                    />
+                                </Card>
+                            </Box>
+                        </Box>
+                    </Box>
                 )}
             </Box>
         </div>
     );
 };
 
-export const getStaticProps = async () => {
-    try {
-        let result = await getAllDocuments("javascript");
-        const javascript = (await result.data) || null;
-        result = await getAllDocuments("css");
-        const css = (await result.data) || null;
-        result = await getAllDocuments("database");
-        const database = (await result.data) || null;
-        return {
-            props: {
-                javascript: javascript.documents,
-                css: css.documents,
-                database: database.documents,
-            },
-            revalidate: 5,
-        };
-    } catch {
-        return {
-            props: {},
-        };
-    }
-};
+// export const getStaticProps = async () => {
+//     try {
+//         let result = await getAllDocuments("javascript");
+//         const javascript = (await result.data) || null;
+//         result = await getAllDocuments("css");
+//         const css = (await result.data) || null;
+//         result = await getAllDocuments("database");
+//         const database = (await result.data) || null;
+//         return {
+//             props: {
+//                 javascript: javascript.documents,
+//                 css: css.documents,
+//                 database: database.documents,
+//             },
+//             revalidate: 5,
+//         };
+//     } catch {
+//         return {
+//             props: {},
+//         };
+//     }
+// };
 
 export default Index;
