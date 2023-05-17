e938873eb4561085c52645bd8990cbae237d27ff
Abdulrahim2567 <kibuh73785569@gmail.com>
2023-05-17 11:58:26 +0100

    moved the skillspage datasource to mongodb

--- src/components/skillspage/rightside/datagrid/DataGrid.jsx
+++ src/components/skillspage/rightside/datagrid/DataGrid.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, useMediaQuery, useTheme } from "@mui/material";
-import { DataGrid as MuiDataGrid } from "@mui/x-data-grid";
+import { DataGrid as MuiDataGrid, GridToolbar } from "@mui/x-data-grid";
 import { SidebarContext } from "@/components/contexts/SidebarContext";
 const DataGrid = (props) => {
     const { javascript, css, database, github, selectedValue } = props;
@@ -10,8 +10,6 @@ const DataGrid = (props) => {
     const [dataGridRowsGithub, setDataGridRowsGithub] = React.useState([]);
 
     const theme = useTheme();
-
-    console.log(javascript);
 
     React.useEffect(() => {
         if (selectedValue === "JavaScript") {
@@ -23,7 +21,6 @@ const DataGrid = (props) => {
                         skill: item.label,
                         proficiency: item.value,
                         dateCreated: new Date("2021-10-10"),
-                        lastLogin: new Date("2021-10-10"),
                     },
                 ]);
             });
@@ -40,7 +37,6 @@ const DataGrid = (props) => {
                         skill: item.label,
                         proficiency: item.value,
                         dateCreated: new Date("2021-10-10"),
-                        lastLogin: new Date("2021-10-10"),
                     },
                 ]);
             });
@@ -57,7 +53,6 @@ const DataGrid = (props) => {
                         skill: item.label,
                         proficiency: item.value,
                         dateCreated: new Date("2021-10-10"),
-                        lastLogin: new Date("2021-10-10"),
                     },
                 ]);
             });
@@ -74,7 +69,6 @@ const DataGrid = (props) => {
                         skill: item.label,
                         proficiency: item.value,
                         dateCreated: new Date("2021-10-10"),
-                        lastLogin: new Date("2021-10-10"),
                     },
                 ]);
             });
@@ -84,30 +78,42 @@ const DataGrid = (props) => {
         }
     }, [selectedValue]);
     const columns = [
-        { field: "skill", headerName: "Skill", width: 100, editable: true },
+        { field: "skill", headerName: "Skill", width: 80, editable: false },
         {
             field: "proficiency",
             headerName: "Proficiency",
             type: "number",
-            editable: true,
+            editable: false,
         },
         {
             field: "dateCreated",
             headerName: "Date Created",
             type: "date",
-            width: 180,
-            editable: true,
-        },
-        {
-            field: "lastLogin",
-            headerName: "Last Login",
-            type: "dateTime",
-            width: 220,
-            editable: true,
+            width: 100,
+            editable: false,
         },
     ];
     return (
-        <Box flexGrow={1} display={"flex"} pt={1.25} width={"100%"}>
+        <Box
+            flexGrow={1}
+            display={"flex"}
+            pt={1.25}
+            width={"100%"}
+            sx={{
+                "& .MuiDataGrid-toolbarContainer": {
+                    backgroundColor: theme.palette.background.alt,
+                    color: theme.palette.secondary.text,
+                    width: "95%",
+                    margin: "auto",
+                    mt: 1,
+                    borderRadius: "5px",
+                    padding: "5px",
+                    "& .MuiButton-root": {
+                        color: theme.palette.secondary.text,
+                    },
+                },
+            }}
+        >
             <MuiDataGrid
                 rows={
                     selectedValue === "JavaScript"
@@ -121,8 +127,6 @@ const DataGrid = (props) => {
                 columns={columns}
                 rowCount={5}
                 sx={{
-                    background: theme.palette.background.paper,
-                    width: "100%",
                     borderColor: theme.palette.background.alt,
                     "& .MuiDataGrid-cell": {
                         borderColor: theme.palette.background.alt,
@@ -130,11 +134,11 @@ const DataGrid = (props) => {
                 }}
                 style={{
                     backgroundColor: theme.palette.background.paper,
-                    width: "100%",
                     height: "365px",
                     borderColor: "transparent",
                     borderRadius: "5px",
                 }}
+                slots={{ toolbar: GridToolbar }}
             />
         </Box>
     );
---
+++ src/data/skills.js
@@ -0,0 +1,151 @@
+export const javascript = [
+    {
+        id: "JavaScript",
+        label: "JavaScript",
+        value: 75,
+        color: "hsl(38, 70%, 50%)",
+        trailColor: "hsl(38, 70%, 50%,0.4)",
+    },
+    {
+        id: "NodeJS",
+        label: "NodeJS",
+        value: 70,
+        color: "hsl(134, 70%, 50%)",
+        trailColor: "hsl(134, 70%, 50%,0.4)",
+    },
+    {
+        id: "ReactJS",
+        label: "ReactJS",
+        value: 70,
+        color: "hsl(216, 70%, 50%)",
+        trailColor: "hsl(216, 70%, 50%,0.4)",
+    },
+    {
+        id: "NextJS",
+        label: "NextJS",
+        value: 80,
+        color: "hsl(310, 70%, 50%)",
+        trailColor: "hsl(310, 70%, 50%,0.4)",
+    },
+    {
+        id: "ExpressJS",
+        label: "ExpressJS",
+        value: 65,
+        color: "hsl(107, 70%, 50%)",
+        trailColor: "hsl(107, 70%, 50%,0.4)",
+    },
+];
+
+export const css = [
+    {
+        id: "CSS",
+        label: "CSS",
+        value: 75,
+        color: "rgb(0, 126, 253)",
+        trailColor: "rgb(0, 126, 253,0.4)",
+    },
+    {
+        id: "Antd",
+        label: "Antd",
+        value: 70,
+        color: "rgb(84, 84, 197)",
+        trailColor: "rgb(84, 84, 197,0.4)",
+    },
+    {
+        id: "NextUI",
+        label: "NextUI",
+        value: 85,
+        color: "hsl(310, 70%, 50%)",
+        trailColor: "hsl(310, 70%, 50%,0.4)",
+    },
+    {
+        id: "Bootstrap",
+        label: "Bootstrap",
+        value: 73,
+        color: "rgb(52, 32, 86)",
+        trailColor: "rgb(52, 32, 86,0.4)",
+    },
+    {
+        id: "HTML",
+        label: "HTML",
+        value: 75,
+        color: "rgb(157, 101, 201)",
+        trailColor: "rgb(157, 101, 201,0.4)",
+    },
+    {
+        id: "MUI5",
+        label: "MUI5",
+        value: 80,
+        color: "hsl(216, 70%, 50%)",
+        trailColor: "hsl(216, 70%, 50%,0.4)",
+    },
+    {
+        id: "Primereact",
+        label: "Primereact",
+        value: 70,
+        color: "rgb(212, 171, 220)",
+        trailColor: "rgb(212, 171, 220,0.4)",
+    },
+];
+
+export const database = [
+    {
+        id: "MongoDB",
+        label: "MongoDB",
+        value: 72,
+        color: "hsl(134, 70%, 50%)",
+        trailColor: "hsl(134, 70%, 50%, 0.4)",
+    },
+    {
+        id: "AWS",
+        label: "AWS",
+        value: 50,
+        color: "hsl(38, 70%, 50%)",
+        trailColor: "hsl(38, 70%, 50%,0.4)",
+    },
+    {
+        id: "Firebase",
+        label: "Firebase",
+        value: 60,
+        color: "hsl(40, 70%, 50%)",
+        trailColor: "hsl(40, 70%, 50%,0.4)",
+    },
+    {
+        id: "MySQL",
+        label: "MySQL",
+        value: 79,
+        color: "hsl(216, 70%, 50%)",
+        trailColor: "hsl(216, 70%, 50%, 0.4)",
+    },
+    {
+        id: "PostgreSQL",
+        label: "PostgreSQL",
+        value: 78,
+        color: "hsl(134, 70%, 50%)",
+        trailColor: "hsl(134, 70%, 50%,0.4)",
+    },
+];
+
+export const github = [
+    {
+        id: "GitHub",
+        label: "GitHub",
+        value: 85,
+        color: "rgb(0,0,0)",
+        trailColor: "rgb(0, 0, 0, 0.4)",
+    },
+    {
+        id: "Git",
+        label: "Git",
+        value: 80,
+        color: "rgb(233, 78, 49)",
+        trailColor: "rgb(233, 78, 49,0.4)",
+    },
+    {
+        id: "Vercel",
+        label: "Vercel",
+        value: 82,
+        color: "rgb(149, 0, 174)",
+        trailColor: "rgb(149, 0, 174, 0.4)",
+    },
+];
