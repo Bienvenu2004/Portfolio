dac07dfe8ee527cec93855f6eca601d136260916
Abdulrarhim2567 <kibuh73785569@gmail.com>
2023-05-18 15:51:19 +0100

    added loaders to the charts on data absent

--- src/components/skillspage/rightside/charts/PieCharts.jsx
+++ src/components/skillspage/rightside/charts/PieCharts.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PieChart from "@/components/charts/PieChart";
 import { Box } from "@mui/material";
+import { Triangle } from "react-loader-spinner";
 
 const PieCharts = ({
     selectedValue,
@@ -10,6 +11,28 @@ const PieCharts = ({
     database,
     github,
 }) => {
+    const [mounted, setMounted] = React.useState(false);
+
+    React.useEffect(() => {
+        setMounted(true);
+    }, [mounted]);
+
+    if (!mounted) {
+        return (
+            <Box display="flex" width="100%">
+                <Triangle
+                    height="80"
+                    width="80"
+                    color="#0091ea"
+                    ariaLabel="triangle-loading"
+                    wrapperStyle={{}}
+                    wrapperClassName=""
+                    visible="true"
+                />
+            </Box>
+        );
+    }
+
     return (
         <Box width="100%">
             {selectedChartValue === "Pie" && selectedValue === "JavaScript" && (
