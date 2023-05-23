e69c7551ecc25f909b94c1f3dd1dd5530e38dcfa
Abdulrahim2567 <kibuh73785569@gmail.com>
2023-05-23 09:50:29 +0100

    added and env variable in the workflow

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,10 +1,10 @@
 import { MongoClient } from "mongodb";
 
-// if (!process.env.MONGODB_URI) {
-//     throw new Error('Invalid environment variable: "MONGODB_URI"');
-// }
+if (!process.env.MONGODB_URI) {
+    throw new Error('Invalid environment variable: "MONGODB_URI"');
+}
 
-const uri = process.env.MONGODB_URI || "test";
+const uri = process.env.MONGODB_URI;
 const options = {};
 
 let client;
