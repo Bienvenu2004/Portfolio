6e807162c05038ed9143f641773e4065e6c2f5fa
Abdulrahim2567 <kibuh73785569@gmail.com>
2023-08-16 13:58:34 -0700

    added some media query to the Top of the profile page

--- src/components/profilepage/top/Top.jsx
+++ src/components/profilepage/top/Top.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import { Box } from "@mui/material";
-import { Button } from "@mui/material";
+import { Box, Button, useMediaQuery } from "@mui/material";
 import {CameraAltRounded } from "@mui/icons-material";
 import { SidebarContext } from "@/components/contexts/SidebarContext";
 const Top = ()=>{
 
+    let coverPhotoWidth = "71%";
+    let screenWidth = 0;
     const {isSidebarOpen} = React.useContext(SidebarContext);
+    const is1109px = useMediaQuery('(max-width:1109px)');
+    const is1230px = useMediaQuery('(max-width:1232px)');
+    
+    is1230px && (coverPhotoWidth = (0.9 * 1230)+"px");
+    is1109px && (coverPhotoWidth = "100%")
+    
 
-    console.log(isSidebarOpen);
+    console.log(screenWidth);
 
     return(
         <Box
             sx={{
                 width:"100%",
-                height:"calc(100vh - 94px)",
+                height:"calc(100vh - 110px)",
                 backgroundImage:"url(/images/me.jpg)",
                 backgroundSize:"cover",
                 backgroundPosition:"center",
@@ -36,6 +43,8 @@ const Top = ()=>{
                         height:"100%",
                         backdropFilter:"blur(200px)",
                         WebkitBackdropFilter:"blur(200px)",
+                        display:"flex",
+                        flexDirection:"column",
                     }}
                 >    
                     <Box
@@ -50,9 +59,9 @@ const Top = ()=>{
                             borderRight:"1px solid transparent",
                             borderBottomRightRadius:"10px",
                             borderBottomLeftRadius:"10px",
-                            width:isSidebarOpen?"83%":"71%",
-                            maxHeight:isSidebarOpen ?  "340px":"390px",
-                            minHeight:isSidebarOpen ?  "340px":"390px",
+                            width:isSidebarOpen?"83%":coverPhotoWidth,
+                            maxHeight:isSidebarOpen ?  "330px":"350px",
+                            minHeight:isSidebarOpen ?  "330px":"350px",
                             margin:"auto",
                             boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
                             transition: "all 0.2s ease-in-out",
@@ -93,6 +102,17 @@ const Top = ()=>{
                                 Edit Cover Photo
                             </Button>
                         </Box>
+                    </Box>
+                    <Box
+                        sx={{
+                            width:isSidebarOpen?"83%":coverPhotoWidth,
+                            border:"1px solid white",
+                            margin:"auto",
+                            display:"flex",
+                        }}
+                        flexGrow={1}
+                    >
+
                     </Box>
                 </Box>
             </Box>
