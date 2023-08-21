bb61c86a34d3080f74fc123d5a2959366a470767
Abdulrahim2567 <kibuh73785569@gmail.com>
2023-08-21 15:04:01 +0100

    working on profile page Avatar

--- src/components/profilepage/top/Top.jsx
+++ src/components/profilepage/top/Top.jsx
@@ -1,103 +1,106 @@
 import React from "react";
 import { Box, Button, useMediaQuery } from "@mui/material";
-import {CameraAltRounded } from "@mui/icons-material";
+import { CameraAltRounded } from "@mui/icons-material";
 import { SidebarContext } from "@/components/contexts/SidebarContext";
-const Top = ()=>{
+import { Avatar } from "@nextui-org/react";
+const Top = () => {
 
     let coverPhotoWidth = "71%";
     let coverPhotoHeight = "350px";
-    const {isSidebarOpen} = React.useContext(SidebarContext);
+    const { isSidebarOpen } = React.useContext(SidebarContext);
     const is1109px = useMediaQuery('(max-width:1109px)');
     const is1230px = useMediaQuery('(max-width:1232px)');
     const is950px = useMediaQuery('(max-width:950px)');
     const is670px = useMediaQuery('(max-width:670px)');
-    
-    is1230px && (coverPhotoWidth = (0.9 * 1230)+"px");
+    const is450px = useMediaQuery('(max-width:450px)')
+
+    is1230px && (coverPhotoWidth = (0.9 * 1230) + "px");
     is1109px && (coverPhotoWidth = "100%")
 
     is950px && (coverPhotoHeight = "300px");
     is670px && (coverPhotoHeight = "250px");
-    
+    is450px && (coverPhotoHeight = "210px")
 
-    return(
+
+    return (
         <Box
             sx={{
-                width:"100%",
-                height:"calc(100vh - 110px)",
-                backgroundImage:"url(/images/me.jpg)",
-                backgroundSize:"cover",
-                backgroundPosition:"center",
-                backgroundRepeat:"no-repeat",
-                overflow:"hidden",
+                width: "100%",
+                height: "fir-content",
+                backgroundImage: "url(/images/me.jpg)",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                overflow: "hidden",
             }}
         >
             <Box
                 className="overlay"
                 sx={{
-                    width:"100%",
-                    height:"100%",
-                    backgroundImage: 'linear-gradient(to bottom, transparent 50%, rgb(0,0,0,0.6) 50%)',
-                    zIndex:-1,
+                    width: "100%",
+                    height: "100%",
+                    backgroundImage: 'linear-gradient(to bottom, transparent 30%, rgb(0,0,0,0.6) 70%)',
+                    zIndex: -1,
                 }}
             >
                 <Box
                     sx={{
-                        width:"100%",
-                        height:"100%",
-                        backdropFilter:"blur(200px)",
-                        WebkitBackdropFilter:"blur(200px)",
-                        display:"flex",
-                        flexDirection:"column",
+                        width: "100%",
+                        height: "100%",
+                        backdropFilter: "blur(200px)",
+                        WebkitBackdropFilter: "blur(200px)",
+                        display: "flex",
+                        flexDirection: "column",
                     }}
-                >    
+                >
                     <Box
                         sx={{
-                            backgroundImage:"url(/images/me.jpg)",
-                            backgroundSize:"cover",
-                            backgroundPosition:"center",
-                            backgroundRepeat:"no-repeat",
-                            borderLeft:"1px solid transparent",
-                            borderBottom:"1px solid transparent",
-                            borderRight:"1px solid transparent",
-                            borderBottomRightRadius:"10px",
-                            borderBottomLeftRadius:"10px",
-                            width:isSidebarOpen?"83%":coverPhotoWidth,
-                            maxHeight:isSidebarOpen ?  "330px":"350px",
-                            minHeight:isSidebarOpen ?  "330px":coverPhotoHeight,
-                            margin:"auto",
+                            backgroundImage: "url(/images/me.jpg)",
+                            backgroundSize: "cover",
+                            backgroundPosition: "center",
+                            backgroundRepeat: "no-repeat",
+                            borderLeft: "1px solid transparent",
+                            borderBottom: "1px solid transparent",
+                            borderRight: "1px solid transparent",
+                            borderBottomRightRadius: "10px",
+                            borderBottomLeftRadius: "10px",
+                            width: isSidebarOpen ? "83%" : coverPhotoWidth,
+                            maxHeight: isSidebarOpen ? "330px" : "350px",
+                            minHeight: isSidebarOpen ? "330px" : coverPhotoHeight,
+                            margin: "auto",
                             boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
                             transition: "all 0.2s ease-in-out",
-                            display:"flex",
-                            justifyContent:"right",
-                            paddingRight:"3%",
-                            
+                            display: "flex",
+                            justifyContent: "right",
+                            paddingRight: "3%",
+
                         }}
                     >
                         <Box
                             sx={{
-                                height:"100%",
-                                minWidth:"170px",
-                                display:"flex",
-                                justifyContent:"end",
-                                flexDirection:"column",
-                                pb:"30px",
+                                height: "100%",
+                                minWidth: "170px",
+                                display: "flex",
+                                justifyContent: "end",
+                                flexDirection: "column",
+                                pb: "30px",
                             }}
                         >
-                            <Button 
-                                variant="contained" 
+                            <Button
+                                variant="contained"
                                 startIcon={<CameraAltRounded />}
                                 sx={{
-                                    height:"fit-content",
-                                    width:"100%",
-                                    backgroundColor:"rgba(0,0,0,0.5)",
-                                    color:"white",
-                                    mb:"5px",
-                                    py:"8px",
-                                    borderRadius:"6px",
-                                    textTransform:"none",
+                                    height: "fit-content",
+                                    width: "100%",
+                                    backgroundColor: "rgba(0,0,0,0.5)",
+                                    color: "white",
+                                    mb: "5px",
+                                    py: "8px",
+                                    borderRadius: "6px",
+                                    textTransform: "none",
                                     transition: "all 0.2s ease-in-out",
-                                    "&:hover":{
-                                        backgroundColor:"rgba(0,0,0,0.4)",
+                                    "&:hover": {
+                                        backgroundColor: "rgba(0,0,0,0.4)",
                                     }
                                 }}
                             >
@@ -107,20 +110,29 @@ const Top = ()=>{
                     </Box>
                     <Box
                         sx={{
-                            width:"100%",
-                            display:"flex",
+                            width: "100%",
+                            display: "flex",
                         }}
                         flexGrow={1}
                     >
                         <Box
                             sx={{
-                                width:isSidebarOpen?"83%":coverPhotoWidth,
-                                border:"1px solid white",
-                                margin:"auto",
-                                display:"flex",
-                                height:'100%'
+                                width: isSidebarOpen ? "83%" : coverPhotoWidth,
+                                border: "1px solid white",
+                                margin: "auto",
+                                display: "flex",
+                                height: '230px'
                             }}
                         >
+                            <Box display="flex" alignItems="center" mr="0.3rem">
+                                <Avatar
+                                    src="/images/me.jpeg"
+                                    css={{ size: "$60", mt: '-120px', ml: '$15' }}
+                                    color={'gradient'}
+                                    bordered
+                                    borderWeight={'bold'}
+                                />
+                            </Box>
 
                         </Box>
                     </Box>
