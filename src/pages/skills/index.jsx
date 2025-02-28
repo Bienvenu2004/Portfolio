40ecfd8c44d8d0da2c35704d4de2604d795d94f3
Abdulrahim2567 <kibuh73785569@gmail.com>
2025-02-28 18:25:18 +0100

    updated skills

--- src/pages/skills/index.jsx
+++ src/pages/skills/index.jsx
@@ -10,7 +10,7 @@ import LeftSide from "@/components/skillspage/leftside/LeftSide";
 import CustomHead from "@/components/head/Head";
 const skills = ["JavaScript", "CSS", "Database", "Git & GitHub"];
 const charts = ["Pie", "Bar", "Line"];
-import { javascript, css, database, github } from "@/data/skills";
+// import { javascript, css, database, github } from "@/data/skills";
 
 const Index = ({ javascript, css, database, github }) => {
     const theme = useTheme();
